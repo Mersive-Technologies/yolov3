9e015446061cf2c118efbacedc2f647aa5918465
Brent Gardner <bgardner@squarelabs.net>
2019-12-22 01:10:47 +0000

    Hyperparamters seem best for training

--- train.sh
+++ train.sh
@@ -1,1 +1,15 @@
-python3 train.py --data data/voc.data --cfg cfg/yolov3-tiny-anchors.cfg --weights weights/yolov3-tiny-ultralytics-pretrained.pt --transfer --rect --device 0
+python3 train.py \
+  --data data/voc.data \
+  --cfg cfg/yolov3-tiny-anchors.cfg \
+  --weights weights/yolov3-tiny-ultralytics-pretrained.pt \
+  --transfer \
+  --rect \
+  --device 0 \
+  --batch-size 64 \
+  --accumulate 1 \
+  --epochs 68 \
+  --img-weights \
+  --adam \
+  --prebias \
+  --multi-scale \
+#  --evolve \
